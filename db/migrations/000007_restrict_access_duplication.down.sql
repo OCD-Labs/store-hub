0cb520f9f26c1e50ffae53f76c1187ee59e63892
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-08-28 14:47:47 +0100

    fix(db): bumped the sales_percentage type to NUMERIC(10, 4)

--- db/migrations/000007_restrict_access_duplication.down.sql
+++ db/migrations/000007_restrict_access_duplication.down.sql
@@ -3,3 +3,6 @@ DROP TRIGGER trigger_distinct_access_levels ON store_owners;
 
 -- Drop the function
 DROP FUNCTION fn_distinct_access_levels();
+
+-- Revert the sales_percentage column
+ALTER TABLE sales_overview ALTER COLUMN sales_percentage TYPE NUMERIC(6, 4);
