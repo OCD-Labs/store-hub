a8362feb04cf67d42950828f5bb0831174b5b323
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-09-13 17:13:51 +0100

    refactor: correct the delete expired session db function

--- db/migrations/000010_update_session_table.up.sql
+++ db/migrations/000010_update_session_table.up.sql
@@ -4,9 +4,22 @@ ALTER TABLE "sessions" ADD CONSTRAINT "unique_token" UNIQUE ("token");
 -- Create an index on the token column for efficient searching
 CREATE INDEX "idx_token" ON "sessions" ("token");
 
+-- Create a new function with the desired return type (in this case, trigger).
+CREATE OR REPLACE FUNCTION public.delete_expired_sessions_trigger()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+  DELETE FROM sessions WHERE expires_at < NOW();
+  RETURN NULL;
+END;
+$function$;
+
 -- Create a trigger that fires before INSERT or UPDATE on the sessions table
 CREATE TRIGGER trigger_delete_expired_sessions
 BEFORE INSERT OR UPDATE
 ON sessions
 FOR EACH ROW
-EXECUTE FUNCTION delete_expired_sessions();
+EXECUTE FUNCTION delete_expired_sessions_trigger();
+
+DROP FUNCTION public.delete_expired_sessions();
